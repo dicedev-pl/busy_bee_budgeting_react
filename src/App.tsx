b4dea66774a897a509f35f1e032ab606660a7256
Robert Szczygielski <szczygielski@dicedev.pl>
2024-03-21 17:07:01 +0100

    feat(add main dashboard): add main dashboard
    add main dashboard after login to application with sider menu
    
    BREAKING CHANGE: -
    
    Closes -
    
    [skip ci]

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ const App = () => {
 
     return (
         <div className="App">
-            <header className="App-header">
-                <MainView/>
-            </header>
+            <MainView/>
         </div>
     );
 }
