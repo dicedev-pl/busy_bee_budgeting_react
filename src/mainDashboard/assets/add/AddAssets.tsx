dad7cd73a9c54194735f1987bafb8c74609a088c
Robert Szczygielski <szczygielski@dicedev.pl>
2024-03-27 19:27:24 +0100

    feat(add docker file + update calendar): add docker file + update calendar
    add docker file + update calendar
    
    BREAKING CHANGE: -
    
    Closes -
    
    [skip ci]

--- src/mainDashboard/assets/add/AddAssets.tsx
+++ src/mainDashboard/assets/add/AddAssets.tsx
@@ -1,22 +1,31 @@
-import {ScheduleOutlined} from '@ant-design/icons';
-import {Input, Select, Space} from 'antd';
+import {DatePicker, Input, Select, Space} from 'antd';
 import React from "react";
 import {AssetsCategory} from "../enums";
+import dayjs from "dayjs";
 
 const {Option} = Select;
 
 const selectBefore = (
-    <Select defaultValue="" style={{ width: 180 }}>
+    <Select defaultValue="" style={{width: 180}}>
         {Object.keys(AssetsCategory).filter(key => isNaN(Number(key))).map((key) => (
             <Option value={key}>{key}</Option>
         ))}
     </Select>
 );
 
+
 const AddAssets = () => {
     return (
         <Space direction="vertical">
-            <Input addonBefore={selectBefore} addonAfter={<ScheduleOutlined/>} defaultValue="0.00"/>
+            <Input addonBefore={selectBefore}
+                   addonAfter={<DatePicker
+                       format="YYYY-MM-DD HH:mm:ss"
+                       showTime={{defaultValue: dayjs('00:00:00', 'HH:mm:ss')}}
+                       style={{ width: 180 }}
+                   />}
+                   defaultValue="0.00"
+                   style={{ width: '100%' }}/>
+
         </Space>
     )
 }
